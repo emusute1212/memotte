2bcabff06af8a2dba100207b04bfd0e5d9b3f932
YosukeMiyanishi <miyanishi1212@gmail.com>
2021-03-18 01:12:46 +0900

    chore(android): make application and appname

---
+++ android/app/src/main/java/io/github/emusute1212/memotte/android/MemotteApplication.kt
@@ -0,0 +1,7 @@
+package io.github.emusute1212.memotte.android
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MemotteApplication : Application()
