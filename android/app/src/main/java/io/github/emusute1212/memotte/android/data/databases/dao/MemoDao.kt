e2382ceb3021d3a6cec7ea43d12623f3e34eb37e
YosukeMiyanishi <miyanishi1212@gmail.com>
2021-03-23 01:15:56 +0900

    chore(android): change room dao return type

--- android/app/src/main/java/io/github/emusute1212/memotte/android/data/databases/dao/MemoDao.kt
+++ android/app/src/main/java/io/github/emusute1212/memotte/android/data/databases/dao/MemoDao.kt
@@ -2,11 +2,12 @@ package io.github.emusute1212.memotte.android.data.databases.dao
 
 import androidx.room.*
 import io.github.emusute1212.memotte.android.data.domain.MemoLocalDbEntity
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface MemoDao {
     @Query("SELECT * FROM memos")
-    suspend fun getAll(): List<MemoLocalDbEntity>
+    suspend fun getAll(): Flow<List<MemoLocalDbEntity>>
 
     @Insert
     suspend fun insert(users: MemoLocalDbEntity)
