3ec26fa80d4a5a6085deaafb97cae081b9dbd101
YosukeMiyanishi <miyanishi1212@gmail.com>
2021-04-06 01:18:20 +0900

    feat(android): fix error

--- android/app/src/main/java/io/github/emusute1212/memotte/android/data/databases/dao/MemoDao.kt
+++ android/app/src/main/java/io/github/emusute1212/memotte/android/data/databases/dao/MemoDao.kt
@@ -7,7 +7,7 @@ import kotlinx.coroutines.flow.Flow
 @Dao
 interface MemoDao {
     @Query("SELECT * FROM memos")
-    suspend fun getAll(): Flow<List<MemoLocalDbEntity>>
+    fun getAll(): Flow<List<MemoLocalDbEntity>>
 
     @Insert
     suspend fun insert(memo: MemoLocalDbEntity)
