24429fdb9bd14355fed94439f3ab9fa54ae61c63
YosukeMiyanishi <miyanishi1212@gmail.com>
2021-04-06 00:37:26 +0900

    feat(android): AppModuleにSingletonComponentを指定

--- android/app/src/main/java/io/github/emusute1212/memotte/android/di/modules/AppModule.kt
+++ android/app/src/main/java/io/github/emusute1212/memotte/android/di/modules/AppModule.kt
@@ -3,11 +3,14 @@ package io.github.emusute1212.memotte.android.di.modules
 import androidx.room.Room
 import dagger.Module
 import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
 import io.github.emusute1212.memotte.android.MemotteApplication
 import io.github.emusute1212.memotte.android.data.databases.MemoDatabase
 import javax.inject.Singleton
 
 @Module
+@InstallIn(SingletonComponent::class)
 object AppModule {
     @Singleton
     @Provides
