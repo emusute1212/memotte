74afe162e1b6805b1e96ed50327333a33fbbae32
YosukeMiyanishi <miyanishi1212@gmail.com>
2021-05-17 00:55:11 +0900

    feat(android): MainActivityのベースを作成

--- android/app/src/main/java/io/github/emusute1212/memotte/android/MainActivity.kt
+++ android/app/src/main/java/io/github/emusute1212/memotte/android/MainActivity.kt
@@ -3,14 +3,15 @@ package io.github.emusute1212.memotte.android
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import dagger.hilt.android.AndroidEntryPoint
-import dagger.hilt.android.lifecycle.HiltViewModel
-import io.github.emusute1212.memotte.android.viewmodel.MemoListViewModel
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val fragment = MemoListFragment()
+        supportFragmentManager.beginTransaction()
+            .add(R.id.fragment_container, fragment)
+            .commit()
         setContentView(R.layout.activity_main)
     }
 }
