4aa5f305317cc39fa8a5bf4a24124020760a9682
YosukeMiyanishi <miyanishi1212@gmail.com>
2021-05-24 00:24:29 +0900

    fix(android): ThreetenAbpの設定を忘れていたので修正

--- android/app/src/main/java/io/github/emusute1212/memotte/android/MemotteApplication.kt
+++ android/app/src/main/java/io/github/emusute1212/memotte/android/MemotteApplication.kt
@@ -1,7 +1,13 @@
 package io.github.emusute1212.memotte.android
 
 import android.app.Application
+import com.jakewharton.threetenabp.AndroidThreeTen
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class MemotteApplication : Application()
+class MemotteApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        AndroidThreeTen.init(this)
+    }
+}
