3be2b4e10763f17709630245370f8a7ebe6d4148
YosukeMiyanishi <miyanishi1212@gmail.com>
2021-05-23 21:46:41 +0900

    feat(android): EditMemoFragmentのクラス作成

---
+++ android/app/src/main/java/io/github/emusute1212/memotte/android/view/edit/EditMemoFragment.kt
@@ -0,0 +1,28 @@
+package io.github.emusute1212.memotte.android.view.edit
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import io.github.emusute1212.memotte.android.databinding.FragmentEditMemoBinding
+import io.github.emusute1212.memotte.android.viewmodel.EditMemoViewModel
+
+class EditMemoFragment : Fragment() {
+    private lateinit var binding: FragmentEditMemoBinding
+
+    private val editMemoViewModel: EditMemoViewModel by activityViewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentEditMemoBinding.inflate(inflater, container, false).also {
+            it.viewModel = editMemoViewModel
+            it.lifecycleOwner = this
+        }
+        return binding.root
+    }
+}
---
+++ android/app/src/main/java/io/github/emusute1212/memotte/android/viewmodel/EditMemoViewModel.kt
@@ -0,0 +1,31 @@
+package io.github.emusute1212.memotte.android.viewmodel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import io.github.emusute1212.memotte.android.usecases.EditMemoUseCase
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+class EditMemoViewModel @Inject constructor(
+    private val editMemoUseCase: EditMemoUseCase
+) : ViewModel() {
+    private var id = INITIALIZE_ID
+    val content = MutableLiveData("")
+
+    fun submitMemo() {
+        content.value?.also {
+            viewModelScope.launch {
+                if (id == INITIALIZE_ID) {
+                    editMemoUseCase.addMemo(it)
+                } else {
+                    editMemoUseCase.editMemo(id, it)
+                }
+            }
+        }
+    }
+
+    companion object {
+        private const val INITIALIZE_ID = -1
+    }
+}
