3379522e8a1554d62f070373bb5948895963af38
YosukeMiyanishi <miyanishi1212@gmail.com>
2021-05-24 00:24:08 +0900

    fix(android): EditMemoViewModelをDagger管理下に入れる

--- android/app/src/main/java/io/github/emusute1212/memotte/android/viewmodel/EditMemoViewModel.kt
+++ android/app/src/main/java/io/github/emusute1212/memotte/android/viewmodel/EditMemoViewModel.kt
@@ -3,10 +3,12 @@ package io.github.emusute1212.memotte.android.viewmodel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
 import io.github.emusute1212.memotte.android.usecases.EditMemoUseCase
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+@HiltViewModel
 class EditMemoViewModel @Inject constructor(
     private val editMemoUseCase: EditMemoUseCase
 ) : ViewModel() {
