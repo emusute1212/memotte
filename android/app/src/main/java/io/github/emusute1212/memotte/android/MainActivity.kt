cf13a8b09d0dbffbdfb871e9fc63685dfa6b6937
YosukeMiyanishi <miyanishi1212@gmail.com>
2021-06-08 01:27:26 +0900

    feat(android): UIの実装

--- android/app/src/main/java/io/github/emusute1212/memotte/android/MainActivity.kt
+++ android/app/src/main/java/io/github/emusute1212/memotte/android/MainActivity.kt
@@ -3,15 +3,19 @@ package io.github.emusute1212.memotte.android
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import dagger.hilt.android.AndroidEntryPoint
+import io.github.emusute1212.memotte.android.view.edit.EditMemoFragment
 import io.github.emusute1212.memotte.android.view.list.MemoListFragment
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val fragment = MemoListFragment()
+        val memoListFragment = MemoListFragment()
+        val editMemoFragment = EditMemoFragment()
+
         supportFragmentManager.beginTransaction()
-            .add(R.id.fragment_container, fragment)
+            .add(R.id.memo_list_fragment, memoListFragment)
+            .add(R.id.edit_fragment, editMemoFragment)
             .commit()
         setContentView(R.layout.activity_main)
     }
