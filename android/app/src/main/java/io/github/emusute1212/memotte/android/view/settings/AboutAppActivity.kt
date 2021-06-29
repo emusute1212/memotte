6dc9e80ead96e8346b2d64a4530ab79e34c19e00
YosukeMiyanishi <miyanishi1212@gmail.com>
2021-06-30 00:37:34 +0900

    feat(android): アプリ情報画面の実装

--- android/app/src/main/java/io/github/emusute1212/memotte/android/view/settings/AboutAppActivity.kt
+++ android/app/src/main/java/io/github/emusute1212/memotte/android/view/settings/AboutAppActivity.kt
@@ -3,17 +3,40 @@ package io.github.emusute1212.memotte.android.view.settings
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.text.util.Linkify
 import androidx.appcompat.app.AppCompatActivity
-import io.github.emusute1212.memotte.android.R
+import io.github.emusute1212.memotte.android.databinding.ActivityAboutAppBinding
 
 class AboutAppActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_about_app)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        ActivityAboutAppBinding.inflate(layoutInflater)
+            .apply {
+                setContentView(this.root)
+                init()
+            }
+    }
+
+    private fun ActivityAboutAppBinding.init() {
+        appVersion.text = packageManager
+            .getPackageInfo(packageName, 0)
+            .versionName
+
+        Linkify.addLinks(
+            appAuthor,
+            PATTERN,
+            MY_PAGE_URL,
+            null
+        ) { _, _ ->
+            MY_PAGE_URL
+        }
     }
 
     companion object {
+        private const val MY_PAGE_URL = "https://www.emusute.com/"
+        private val PATTERN = "Yosuke Miyanishi".toPattern()
+
         private fun createIntent(context: Context): Intent {
             return Intent(context, AboutAppActivity::class.java)
         }
