4574266811690c75b7bc5a7b6f255156f92fb0a3
YosukeMiyanishi <miyanishi1212@gmail.com>
2021-08-20 01:48:38 +0900

    chore(android): リリース署名のデバッグを無効に

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -30,7 +30,6 @@ android {
         getByName("release") {
             isMinifyEnabled = true
             isShrinkResources = true
-            isDebuggable = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
